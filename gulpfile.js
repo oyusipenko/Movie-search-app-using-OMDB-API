c2248e0dd869b5ed8ab04e609a8d7d415f411546
oyusipenko <o.yusipenko@gmail.com>
2020-08-14 16:14:27 +0300

    1

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+const gulp = require('gulp');
+const brSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('brSync', () => {
+    brSync.init({
+        server: {          
+            baseDir: "./public"
+        },
+    });
+});
+
+gulp.task('html', () => {   
+    return gulp.src('public/*.html', { since: gulp.lastRun('html') })
+        .pipe(brSync.reload({ stream: true }))  // перезагрузка страницы 
+});
+
+gulp.task('sass', () => {
+    return gulp.src('app/common.blocks/style.scss')
+        .pipe(sourcemaps.init())        // активируем gulp-sourcemaps 
+        .pipe(sass({
+            outputStyle: 'nested',
+        })
+            .on('error', sass.logError))
+        .pipe(sourcemaps.write())   // создание карты css.map в текущей папке 
+        .pipe(gulp.dest('public/css'))
+        .pipe(brSync.reload({ stream: true }))
+});
+
+gulp.task('watch', gulp.parallel('brSync', () => {
+    gulp.watch('app/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('public/*.html', gulp.parallel('html')) // следим за изменениями HTML 
+}));
+
+gulp.task('default', gulp.parallel('html', 'watch', 'sass')); // задача по умолчанию (gulp)
